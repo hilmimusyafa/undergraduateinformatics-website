7d6e3ea23d1b66b130bfa3593da0141e99d9bf38
rafidhn06 <rafidhn06@gmail.com>
2026-09-02 02:21:36 +0700

    test: add TextButton ladle stories

--- resources/js/components/Buttons.stories.tsx
+++ resources/js/components/Buttons.stories.tsx
@@ -3,6 +3,7 @@ import type { Story } from '@ladle/react';
 import { PrimaryButton } from './PrimaryButton';
 import { SearchBar } from './SearchBar';
 import { SecondaryButton } from './SecondaryButton';
+import { TextButton } from './TextButton';
 
 export default {
     title: 'Primitives/Buttons',
@@ -13,3 +14,11 @@ export const Primary: Story = () => <PrimaryButton>Kirim</PrimaryButton>;
 export const Secondary: Story = () => <SecondaryButton>Kembali</SecondaryButton>;
 
 export const Search: Story = () => <SearchBar />;
+
+export const TextFade: Story = () => <TextButton variant="fade">Kembali</TextButton>;
+
+export const TextUnderline: Story = () => (
+    <TextButton variant="underline" className="text-blue-600">
+        Isi Formulir Lagi
+    </TextButton>
+);
