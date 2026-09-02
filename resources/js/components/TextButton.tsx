0b28e7831cb5e4aa60df53144d86f7fac67599aa
rafidhn06 <rafidhn06@gmail.com>
2026-09-02 14:16:07 +0700

    fix: reset TextButton padding

--- resources/js/components/TextButton.tsx
+++ resources/js/components/TextButton.tsx
@@ -12,7 +12,7 @@ export function TextButton({ className, variant, ...props }: TextButtonProps) {
         <Button
             variant={variant === 'underline' ? 'link' : 'ghost'}
             className={cn(
-                'h-auto px-3 py-1.5 text-base',
+                'h-auto p-0 text-base',
                 variant === 'fade' &&
                     'text-foreground hover:text-muted-foreground hover:bg-transparent dark:hover:bg-transparent',
                 className
