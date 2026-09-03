c53c8c9908507e569052da4c9d89ad8aafa07a0d
rafidhn06 <rafidhn06@gmail.com>
2026-09-03 07:03:56 +0700

    feat: derive page spacing from content grid gutter

--- resources/js/components/ArticleContainer.tsx
+++ resources/js/components/ArticleContainer.tsx
@@ -8,7 +8,7 @@ export function ArticleContainer({ className, ...divProps }: ArticleContainerPro
     return (
         <div
             className={cn(
-                'typeset typeset-article mx-auto w-full max-w-[37em] px-4 py-10 md:py-9',
+                'typeset typeset-article mx-auto w-full max-w-[37em] py-10 md:py-9',
                 className
             )}
             {...divProps}
