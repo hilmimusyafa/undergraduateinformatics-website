c9d6e090a37d9cad14b47b027bf291db6609cae5
rafidhn06 <rafidhn06@gmail.com>
2026-09-06 02:21:24 +0700

    fix: refine tags page skeleton layout

--- resources/js/features/tags/TagsStates.tsx
+++ resources/js/features/tags/TagsStates.tsx
@@ -5,17 +5,17 @@ export function TagsSkeleton() {
     return (
         <ArticleContainer role="status" aria-label="Memuat daftar label" className="max-w-[37em]">
             <h1>
-                <Skeleton className="h-9 w-full" />
+                <Skeleton className="h-9 w-1/2" />
             </h1>
             <div className="flex flex-col gap-1">
                 <Skeleton className="h-7 w-full" />
                 <Skeleton className="h-7 w-full" />
             </div>
             <ul className="list-none">
-                {[0, 1, 2, 3, 4, 5].map((index) => (
+                {[0, 1, 2, 3, 4, 5, 6].map((index) => (
                     <li key={index} className="relative">
-                        <Skeleton className="absolute top-2 -left-5.5 size-2" />
-                        <Skeleton className="h-7 w-full" />
+                        <Skeleton className="absolute top-2 -left-5 size-2" />
+                        <Skeleton className="h-7 w-1/2" />
                         <p>
                             <Skeleton className="h-7 w-full" />
                         </p>
